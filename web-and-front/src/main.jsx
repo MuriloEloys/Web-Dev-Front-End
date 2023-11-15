6c4591e23496844aa7dae25a55908fcf5420e584
MuriloEloys <rm552117@fiap.com.br>
2023-11-15 14:09:44 -0300

    Geração do cabecalho e rodape

--- web-and-front/src/main.jsx
+++ web-and-front/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/Home.jsx'
+import Erro404 from './routes/Erro404.jsx'
+
+
+const router = createBrowserRouter([
+  {path:"/",element: <App/>, errorElement:<Erro404/>,
+  children:[
+    {path:"/", element:<Home/>},
+    // {path:"/login",element:<Login/>},
+    // {path:"/cadastro",element:<Cadastro/>},
+  ]
+ }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
 )
---
+++ web-and-front/src/routes/Home/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return(
+        <>
+            <h1>VOCÊ ESTÁ NA HOME!!!!</h1>
+        </>
+    )
+}
